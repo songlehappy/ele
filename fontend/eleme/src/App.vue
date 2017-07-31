cc4b860563e2717c83add89b3e57af334d5900c3
Arthur Xiao <762021105@.com>
2017-07-31 20:15:24 +0800

    ArthurXiao0731

--- fontend/eleme/src/App.vue
+++ fontend/eleme/src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <Discover></Discover>
   </div>
 </template>
 
 <script>
+import Discover from './components/FRouter.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components:{
+    Discover
+  }
 }
 </script>
 
--- fontend/eleme/src/router/index.js
+++ fontend/eleme/src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Msite from '@/components/Msite'
-
+import Discover from '@/components/Discover'
+import Profile from '@/components/Profile'
+import Order from '@/components/Order'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/msite',
       name: 'Msite',
       component: Msite
+    },
+    {
+      path:'/discover',
+      name:'Discover',
+      component:Discover
+    },
+     {
+      path:'/profile',
+      name:'Discover',
+      component:Profile
+    },
+     {
+      path:'/order',
+      name:'Order',
+      component:Order
     },
     {
       path:"*",
